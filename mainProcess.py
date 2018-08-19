15421ba7cc7f29f370f6a3bc9ef1ddb403f4f495
kono <konotorie@gmail.com>
2018-08-19 22:50:18 +0900

    activate JPN stocks

--- mainProcess.py
+++ mainProcess.py
@@ -4,9 +4,9 @@ def generateText():
 
     USStocks = ['AAPL','F']
     text = getFromIEX(USStocks)
-    # JapaneseStocks = {'Rakuten': '4755', 'Tokyu': '8957', 'Fukuoka': '8968'}
-    # text += '------------------------\n'
-    # text += getFromScraping(JapaneseStocks)
+    JapaneseStocks = {'Rakuten': '4755', 'Tokyu': '8957', 'Fukuoka': '8968'}
+    text += '------------------------\n'
+    text += getFromScraping(JapaneseStocks)
     return text
 
 generateText()
