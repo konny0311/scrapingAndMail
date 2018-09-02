fb49b965fcea1ee2d1f16eef78e6e0a5f0d90214
kono <konotorie@gmail.com>
2018-09-02 11:28:31 +0900

    limit exec days in weekdays

--- regularExec/regularExecJapan.py
+++ regularExec/regularExecJapan.py
@@ -1,11 +1,14 @@
 import sys
 sys.path.append('../')
 from mainProcess import *
+import datetime
 
 #This code is executed to get stock info everyday.
 #exec this code every 15:30pm Japan time
-manager = MongoDBManager.MongoDBManager()
-stockDB = manager.getDB("stock")
-japanCompanyList = getCompanyList(manager, 'japan')
-if japanCompanyList is not None:
-    getFromNikkeiScraping(japanCompanyList)
+dayOfWeek = datetime.datetime.today().weekday()
+if 0 <= dayOfWeek and dayOfWeek <= 4: #work in weekdays
+    manager = MongoDBManager.MongoDBManager()
+    stockDB = manager.getDB("stock")
+    japanCompanyList = getCompanyList(manager, 'japan')
+    if japanCompanyList is not None:
+        getFromNikkeiScraping(japanCompanyList)
--- regularExec/regularExecUs.py
+++ regularExec/regularExecUs.py
@@ -4,8 +4,10 @@ from mainProcess import *
 
 #This code is executed to get stock info everyday.
 #exec this code every 6:00am Japan time
-manager = MongoDBManager.MongoDBManager()
-stockDB = manager.getDB("stock")
-usCompanyList = getCompanyList(manager, 'us')
-if usCompanyList is not None:
-    getFromYFUSScraping(usCompanyList)
+dayOfWeek = datetime.datetime.today().weekday()
+if 1 <= dayOfWeek and dayOfWeek <= 5: #work in weekdays
+    manager = MongoDBManager.MongoDBManager()
+    stockDB = manager.getDB("stock")
+    usCompanyList = getCompanyList(manager, 'us')
+    if usCompanyList is not None:
+        getFromYFUSScraping(usCompanyList)
